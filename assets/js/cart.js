22b604a35ed942b3289f63c88e73a423cc2735c9
Alejandra Caceres <caceres299romina@hotmail.com>
2025-06-18 21:01:04 -0300

    filtros añadidos

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -8,3 +8,25 @@ document.addEventListener("DOMContentLoaded", function() {
         cards[index].classList.add('active');
     }, 5000); // Cambia el número (en milisegundos) para ajustar la duración entre cambios
 });
+
+// maneja los filtros de productos 
+  document.addEventListener("DOMContentLoaded", function () {
+    const filtros = document.querySelectorAll(".filtro-btn");
+    const productos = document.querySelectorAll(".product-box");
+
+    filtros.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const filtro = btn.getAttribute("data-filtro");
+
+        productos.forEach((producto) => {
+          const categoria = producto.getAttribute("data-categoria");
+
+          if (categoria === filtro) {
+            producto.style.display = "block";
+          } else {
+            producto.style.display = "none";
+          }
+        });
+      });
+    });
+  });
